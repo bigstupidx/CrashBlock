c8a3cb80fe0ef732386d0fa4b2fea11e850bed4c
Ernesto <ernesto.solares.guerrero@gmail.com>
2016-12-28 17:09:04 -0600

    Ammo Pickups && Canvas UI

---
+++ Assets/models/Ammo packs/Materials/rotateItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateItem : MonoBehaviour {
+
+	public float rotx = 50;
+	public float roty = 50;
+	public float rotz = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(Time.deltaTime* rotx,Time.deltaTime*roty,Time.deltaTime*rotz));
+	
+	}
+}
