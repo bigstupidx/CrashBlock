77813d8748fc792bc907f305d5994d956573ed42
John Delvalle <john.laad@gmail.com>
2017-01-08 14:58:47 -0600

    UI y pausa

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+using ControlFreak2.Internal;
+
+namespace ControlFreak2
+{
+public class PauseManager : MonoBehaviour {
+
+
+	public TouchButton touchbot;
+
+	public bool isPaused;
+
+
+
+		void update ()
+		{
+
+
+		}
+	
+	
+	
+	}
+}
