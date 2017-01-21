bc53cbc55bcd51b2278889e256d3ea5e980309bb
John Delvalle <john.laad@gmail.com>
2017-01-20 22:21:08 -0600

    Boton de menu fixed
    Logo ya avanza  a menu y menu a level select

---
+++ Assets/_Scripts/AutoLoadLevel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AutoLoadLevel : MonoBehaviour {
+
+	[SerializeField]
+	private string nextLevel;
+	[SerializeField]
+	private float loadingTime = 3.0f;
+
+
+	void Start () 
+	{
+		StartCoroutine (LoadNextLevelCo());
+	
+	}
+
+	//  load next level after 3 seconds.
+
+	IEnumerator LoadNextLevelCo()
+	{
+
+
+		yield return new WaitForSeconds (loadingTime);
+
+
+		SceneManager.LoadScene ("" + nextLevel, LoadSceneMode.Single);
+
+	}
+
+
+
+
+
+}
