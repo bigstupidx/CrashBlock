5f626715b9a9efb424ed10079469adc16097f5c6
Ernesto <ernesto.solares.guerrero@gmail.com>
2017-05-16 17:07:27 -0500

    Block Soldier survival games 90%

---
+++ Assets/Editor/MassSetMaterials.js
@@ -0,0 +1,11 @@
+﻿@MenuItem("Scripts/Mass Set Materials")
+static function MassSetMaterials() {
+    Undo.RegisterSceneUndo("Mass Set Materials");
+
+     var mats : Material[] = Selection.activeGameObject.GetComponent.<Renderer>().sharedMaterials;
+     for (var obj : GameObject in Selection.gameObjects) {
+         obj.GetComponent.<Renderer>().sharedMaterials = mats;
+     }
+
+
+}
