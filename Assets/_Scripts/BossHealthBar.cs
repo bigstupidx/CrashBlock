eb85b326f591e5b15f4684a7ea29e2a3583b7b03
Ernesto <ernesto.solares.guerrero@gmail.com>
2018-01-14 23:47:54 -0600

    level 4 finished

---
+++ Assets/_Scripts/BossHealthBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BossHealthBar : MonoBehaviour {
+
+    public Image bossBar;
+    public CharacterDamage bossEnemy;
+    float maxHealthOfBoss;
+
+    private void Start()
+    {
+        maxHealthOfBoss = bossEnemy.hitPoints;
+    }
+
+    void Update()
+    {
+        bossBar.fillAmount = Mathf.InverseLerp(0, maxHealthOfBoss, bossEnemy.hitPoints);
+    }
+}
